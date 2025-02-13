afcf8263804cc401ebf696886cb7c8fa7e9ee34c
Jean Lima <jeanlima5939@gmail.com>
2025-02-13 01:03:09 -0300

    update in Header

--- site/src/components/header/Header.tsx
+++ site/src/components/header/Header.tsx
@@ -1,27 +1,85 @@
 "use client";
 
-
 import Link from "next/link";
- import Image from "next/image";
+import Image from "next/image";
+import { useState } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@/components/ui/breadcrumb";
+import { MdLanguage } from "react-icons/md";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
-
+  const [language, setLanguage] = useState("pt");
 
   return (
-    <header className="bg-primary text-white flex items-center justify-between px-4 h-20">     
-        <Link href="/" className="flex items-center ">
-          <Image 
-            src="/assets/logo/image-removebg-preview.png" 
-            alt="Logo" 
-            width={100} 
-            height={100} 
+    <header className="bg-primary text-white flex items-center pr-20 h-16 justify-between shadow-md">
+      <div className="flex items-center">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/assets/logo/image-removebg-preview.png"
+            alt="Logo"
+            width={100}
+            height={100}
             className="rounded-full"
           />
-          <h1 className="text-2xl  text-secondary">
-            SchemasterDB</h1>
+          <h1 className="text-2xl text-secondary ">SchemasterDB</h1>
         </Link>
-        
-       
+      </div>
+
+      <Breadcrumb className="flex justify-around text-[20px] font-bold w-1/2 ">
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Início</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/dashboard">Funcionalidades</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/dashboard">Sobre</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/dashboard">Integrantes</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="bg-primary text-white border-secondary "
+          >
+            <MdLanguage /> {language.toUpperCase()}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 bg-text text-textDark">
+          <DropdownMenuLabel className="text-[#496B75]">
+            Selecione o idioma
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator className="bg-primary" />
+          <DropdownMenuRadioGroup value={language} onValueChange={setLanguage}>
+            <DropdownMenuRadioItem value="pt" className="hover:bg-background">
+              Português
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="en" className="hover:bg-background">
+              Inglês
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="es" className="hover:bg-background">
+              Espanhol
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
--- site/src/components/ui/breadcrumb.tsx
+++ site/src/components/ui/breadcrumb.tsx
@@ -9,14 +9,7 @@ const Breadcrumb = React.forwardRef<
   React.ComponentPropsWithoutRef<"nav"> & {
     separator?: React.ReactNode
   }
->(({ className, ...props }, ref) => (
-  <nav
-    ref={ref}
-    aria-label="breadcrumb"
-    className={cn("bg-[#D4D4D4] p-2 rounded-md", className)}
-    {...props}
-  />
-))
+>(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />)
 Breadcrumb.displayName = "Breadcrumb"
 
 const BreadcrumbList = React.forwardRef<
@@ -26,7 +19,7 @@ const BreadcrumbList = React.forwardRef<
   <ol
     ref={ref}
     className={cn(
-      "flex flex-wrap items-center gap-2 text-sm text-[#FFFFFF]",
+      "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
       className
     )}
     {...props}
@@ -38,7 +31,11 @@ const BreadcrumbItem = React.forwardRef<
   HTMLLIElement,
   React.ComponentPropsWithoutRef<"li">
 >(({ className, ...props }, ref) => (
-  <li ref={ref} className={cn("inline-flex items-center gap-2", className)} {...props} />
+  <li
+    ref={ref}
+    className={cn("inline-flex items-center gap-1.5", className)}
+    {...props}
+  />
 ))
 BreadcrumbItem.displayName = "BreadcrumbItem"
 
@@ -53,10 +50,7 @@ const BreadcrumbLink = React.forwardRef<
   return (
     <Comp
       ref={ref}
-      className={cn(
-        "text-[#15242B] transition-colors hover:text-[#E56024] font-medium",
-        className
-      )}
+      className={cn("transition-colors hover:text-secondary", className)}
       {...props}
     />
   )
@@ -72,7 +66,7 @@ const BreadcrumbPage = React.forwardRef<
     role="link"
     aria-disabled="true"
     aria-current="page"
-    className={cn("font-bold text-[#38A169]", className)}
+    className={cn("font-normal text-foreground", className)}
     {...props}
   />
 ))
@@ -86,7 +80,7 @@ const BreadcrumbSeparator = ({
   <li
     role="presentation"
     aria-hidden="true"
-    className={cn("[&>svg]:w-4 [&>svg]:h-4 text-[#D69E2E]", className)}
+    className={cn("[&>svg]:w-3.5 [&>svg]:h-3.5", className)}
     {...props}
   >
     {children ?? <ChevronRight />}
@@ -101,17 +95,14 @@ const BreadcrumbEllipsis = ({
   <span
     role="presentation"
     aria-hidden="true"
-    className={cn(
-      "flex h-9 w-9 items-center justify-center text-[#4A5568] bg-[#2C7A7B] rounded-md",
-      className
-    )}
+    className={cn("flex h-9 w-9 items-center justify-center", className)}
     {...props}
   >
     <MoreHorizontal className="h-4 w-4" />
     <span className="sr-only">More</span>
   </span>
 )
-BreadcrumbEllipsis.displayName = "BreadcrumbEllipsis"
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis"
 
 export {
   Breadcrumb,
